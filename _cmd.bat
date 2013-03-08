4385eff52621fed9ff002f00248d0c485bbb4362
Jin-Wook Chung <jwChung@hotmail.com>
2013-03-09 01:12:54 +0900

    Downgraded python to v2.7.3.2.

--- _cmd.bat
+++ _cmd.bat
@@ -1,3 +1,3 @@
 @echo off
-set PATH=C:\Portable Python 3.2.1.1\App\Scripts\;C:\Portable Python 3.2.1.1\App\;C:\Ruby200-x64\bin;C:\Program Files (x86)\Git\bin;%PATH%
+set PATH=C:\Portable Python 2.7.3.2\App\Scripts\;C:\Portable Python 2.7.3.2\App\;C:\Ruby200-x64\bin;C:\Program Files (x86)\Git\bin;%PATH%
 cmd
