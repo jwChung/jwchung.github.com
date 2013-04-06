3c2824a5a4834383dc607a9c303801e5a69d755d
Jin-Wook Chung <jwChung@hotmail.com>
2013-04-07 01:05:35 +0900

    Modified the python path.

--- _cmd.bat
+++ _cmd.bat
@@ -1,3 +1,3 @@
 @echo off
-set PATH=C:\Portable Python 2.7.3.2\App\Scripts\;C:\Portable Python 2.7.3.2\App\;C:\Ruby200-x64\bin;C:\Program Files (x86)\Git\bin;%PATH%
+set PATH=C:\Python27;C:\Python27\Scripts;C:\Ruby200-x64\bin;C:\Program Files (x86)\Git\bin;%PATH%
 cmd
